00333f695e1ea325ff42be0bbfb8afec41b32f7c
Cavid Bashirov <bashirovcavid93gmail.com>
2022-04-07 18:14:25 +0400

    Added some packages and context configurations

---
+++ Domain/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DatabaseFacade GetDatabase()
+        {
+            return base.Database;
+        }
+
+        public DbSet<TEntity> GetDbSet<TEntity>() where TEntity : BaseEntity
+        {
+            return Set<TEntity>();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditingEntity>())
+            {
+                var utcNow = DateTime.UtcNow;
+
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = 0;
+                        entry.Entity.CreatedAt = utcNow;
+                        entry.Entity.LastModifiedBy = 0;
+                        entry.Entity.LastModifiedAt = utcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Property(e => e.CreatedBy).IsModified = false;
+                        entry.Property(e => e.CreatedAt).IsModified = false;
+                        entry.Entity.LastModifiedBy = 0;
+                        entry.Entity.LastModifiedAt = utcNow;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ Domain/Common/IAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public interface IAppDbContext
+    {
+        public DatabaseFacade GetDatabase();
+        public DbSet<TEntity> GetDbSet<TEntity>() where TEntity : BaseEntity;
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = new());
+    }
+}
